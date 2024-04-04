cdaadc21a046fca6e983d63dc31f795faf7dbf36
Mandiegr <nannda076@gmail.com>
2024-04-03 23:36:21 -0300

    feat: add colors and frames

--- calculadora/index.py
+++ calculadora/index.py
@@ -1,8 +1,21 @@
 from tkinter import *
 from tkinter import ttk
 
-window =Tk()
-window.title("calculator")
-window.geometry("235x318")
+cor1 = "#363636"
+cor2 = "#feff"
+cor3 = "#38576b"
+cor4 = "#ECEFF1"
+cor5 = "#FFAB40"
 
-window.mainloop()
+janela =Tk()
+janela.title("calculator")
+janela.geometry("235x318")
+janela.config(bg=cor1)
+
+frame_tela = Frame(janela, width=235, height=50, bg=cor3)
+frame_tela.grid(row=0, column=0)
+
+frame_corpo = Frame(janela, width=235, height=268)
+frame_corpo.grid(row=1, column=0)
+
+janela.mainloop()  
